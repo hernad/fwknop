ba0ade97522dbdc640f84b3152c709ebb102c387
hernad <hernad@bring.out.ba>
2017-05-25 19:04:02 +0200

    debug

--- server/fw_util_iptables.c
+++ server/fw_util_iptables.c
@@ -1630,7 +1630,7 @@ process_spa_request(const fko_srv_options_t * const opts,
         /* Create an access command for each proto/port for the source ip.
         */
 
-       if (strncasecmp(opts->config[CONF_ENABLE_DELETE_OLD_RULES], "Y", 1) == 0) {
+       //if (strncasecmp(opts->config[CONF_ENABLE_DELETE_OLD_RULES], "Y", 1) == 0) {
 
           // iptables -L -v --line-numbers | grep ACCEPT.*192.168.168.194.*_exp_ | awk '{ print $1 }' | sort -nr | xargs -n 1 sudo iptables -D FWKNOP_INPUT
           snprintf(cmd_buf, CMD_BUFSIZE-1,
@@ -1639,7 +1639,7 @@ process_spa_request(const fko_srv_options_t * const opts,
           res = run_extcmd(cmd_buf, err_buf, CMD_BUFSIZE, WANT_STDERR, NO_TIMEOUT, &pid_status, opts);
           log_msg(LOG_DEBUG, "ENABLE_DELETE_OLD_RULES CMD: '%s' (res: %d, err: %s)",
               cmd_buf, res, err_buf);
-        }
+        //}
 
         while(ple != NULL)
         {
